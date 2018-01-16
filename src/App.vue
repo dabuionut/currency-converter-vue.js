3e070469c6486f20ede73c5fc13f69764e5a60b2
Dabu <ionut.dabu@accenture.com>
2018-01-16 11:10:39 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,150 @@
+<template>
+  <div class="row" id="app">
+
+<!-- Nav Bar -->      
+<nav class="navbar navbar-default">
+  <div class="container-fluid"> 
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Currency Converter</a>
+    </div>
+
+   
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Link</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<!-- Nav Bar -->
+
+<div class="container">
+    <div class="col-md-12">
+    <div class="panel panel-default">
+        <div id="header-color" class="panel-heading">
+            <h3 id="title-text" class="title-panel">Currency Converter</h3> 
+            <span class="flag-icon flag-icon-us"></span>
+            <span class="flag-icon flag-icon-ro"></span>
+            <span class="flag-icon flag-icon-es"></span>
+            <span class="flag-icon flag-icon-de"></span>
+        </div>
+    	<currency></currency>
+    </div>
+    </div>
+    <div class="col-md-12">
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h3 class="title-panel">Your latest conversions</h3> 
+        </div>
+    <div class="col-xs-12 col-sm-12 col-md-12">    
+    <div class="button-convert">
+    <button v-bind="newCurr" id="store-rate" type="button" class="btn btn-success" @click="addCurr">Store Conversions</button>
+    </div>
+    <div class="button-convert">
+    <button id="store-rate" type="button" class="btn btn-danger" @click="clearList">Clear List</button>
+    </div>
+    </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+        <div id="alert-box" class="alert alert-warning">
+          <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+          <strong>Notice!</strong> You can save up to 100 conversions, wich will be stored for 30 days.
+        </div>
+        </div>
+<table id="tablestore" class="table table-bordered">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Date</th>
+      <th scope="col">Conversions</th>
+    </tr>
+  </thead>
+  <tbody v-bind="curr in currs">
+    <tr>
+      <th scope="row">1</th>
+      <td></td>
+      <td>{{ curr.text }}</td>
+      <!--<button class="delete" @click="removeCurr(curr)">X</button>-->
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td></td>
+      <td>{{ curr.text }}</td>
+      <!--<button class="delete" @click="removeCurr(curr)">X</button>-->
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td></td>
+      <td>{{ curr.text }}</td>
+      <!--<button class="delete" @click="removeCurr(curr)">X</button>-->
+    </tr>
+  </tbody>
+</table>
+      <h5 class="u-text-center"></h5>
+    </div>
+    </div>  
+  </div>
+  </div>
+</template>
+
+<script>
+import Currency from './components/Currency'
+
+export default {
+  name: 'app',
+  components: {
+    Currency
+  },
+  data () {
+    return {
+      newCurr: "",
+      currs: [
+			],
+      curr : ""
+    }
+  },
+  computed: {
+			areAllSelected: function () {
+				return this.currs.every(function(curr) {
+					return curr.checked;
+				}) &&  this.currs.length > 0;
+			},
+		},
+  methods: {
+    addCurr: function () {
+				var curr = this.newCurr.trim();
+        console.log( Currency );
+				if (curr) {
+					this.currs.push({text: curr});
+					this.newCurr = "";
+          console.log(this.currs);
+				}
+		},
+    removeCurr: function (curr) {
+        var index = this.currs.indexOf(curr);
+				this.currs.splice(index, 1);
+		},
+    clearList: function () {
+				this.currs = [
+
+				];
+		},
+    // showText: function(val) {      
+    //   console.log(val);
+    // }
+  }
+
+}
+
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Currency.vue
@@ -0,0 +1,141 @@
+<template>
+  <div class="hello">
+    <h3 class="u-text-center main-heading">{{ title }}</h3>
+
+
+    <div class="row">
+      <div>
+        <label>Amount</label>
+        <input min="1" v-on:change="convertRates" v-on:keyup="convertRates" type="number" name="base_amount" v-model="base_amount">
+      </div>
+    </div>
+    <div id="topmove" class="row">
+      <div class="one-half column">
+        <label>From</label>
+        <symbol-list v-on:symbolChange="symbolChange" :list="baseList()" name="base" :initialvalue="base"></symbol-list>
+      </div>
+      <div class="one-half column">
+        <label>To</label>
+        <symbol-list v-on:symbolChange="symbolChange" :list="targetList()" name="target" :initialvalue="target"></symbol-list>
+      </div>
+    </div>
+    <div class="row">
+      <div>
+        <h5 v-if="base_amount" class="target-amount u-text-center">{{ base_amount + base.name + '=' + target_amount + target.name }}</h5>
+        <!--<input type="hidden" :value="{{ showText(base_amount + base.name + '=' + target_amount + target.name) }} " id="getCurr" />-->
+      </div>
+    </div>  
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import SymbolList from './SymbolList';
+const fixerUrl = 'http://api.fixer.io/latest'
+
+function getRates (base) {
+  return axios.get(fixerUrl, {
+    params: {
+      base: base
+    }
+  }).then(res => {
+
+    const {data} = res;
+    var currency_list = [];
+
+    currency_list.push({
+      name: data.base,
+      value: 1
+    });
+
+    for(var rate in data.rates) {
+      currency_list.push({
+        name: rate,
+        value: data.rates[rate]
+      });
+    }
+
+    return currency_list
+  
+  })
+}
+
+function convertRates (base, target) {
+  return axios.get(fixerUrl, {
+    params: {
+      base: base,
+      symbols: target
+    }
+  }).then(res => {
+
+    const {data} = res;
+    var currency_list = [];
+
+    return data.rates[target];
+  
+  })
+}
+
+export default {
+  name: 'currency',
+  components: {
+    SymbolList
+  },
+  data () {
+    return {
+      title: 'Convert Currency',
+      currency_list: [],
+      base: '',
+      target: '',
+      base_amount: 1,
+      target_amount: 1,
+      props: [ "base_amount", "base.amount", "target_amount", "target.name"],
+      text: "",
+      span: ""
+    }
+  },
+  created: function () {
+
+    getRates('USD')
+      .then(res => {
+        this.currency_list = res;
+        this.base = res[0];
+        this.target = res[1];
+      })
+    
+  },
+  methods: {
+    symbolChange: function (val, name) {
+      this[name] = val;
+      this.convertRates()
+    },
+    convertRates: function () {
+
+      if(!this.base_amount) {
+        return;
+      }
+
+      convertRates(this.base.name, this.target.name)
+        .then(res => this.target_amount = res * parseInt(this.base_amount, 10));
+    },
+    baseList: function () {
+      return this.currency_list.filter(currency => currency.name != this.target.name)
+    },
+    targetList: function () {
+      return this.currency_list.filter(currency => currency.name != this.base.name)
+    },
+    showText: function(text) {
+      return this.span = text;
+      console.log(text);
+    }
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+
+
+</style>
